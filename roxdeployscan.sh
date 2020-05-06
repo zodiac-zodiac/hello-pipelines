1acf4baf754faed4b39db863839328d835a7f999
Amari nader <amari.nader@gmail.com>
2020-05-06 17:06:14 -0400

    Delete roxdeployscan.sh

--- roxdeployscan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#curl -s -k -H "Authorization: Bearer ${ROX_API_TOKEN}" https://${CENTRAL}/api/cli/download/roxctl-linux --output roxctl
-sudo chmod +x ./roxctl
-
-#export ROX_API_TOKEN=${ROX_API_TOKEN}
-sudo ./roxctl deployment check --insecure-skip-tls-verify -e ${CENTRAL} --file ${KUBEYAML}
-
-exit $?
