b073bc5f4c88422f93b06f544cc4fca218ff3b2b
Amari nader <amari.nader@gmail.com>
2020-05-06 15:27:49 -0400

    Update roximagescan.sh
    add sudo privilege

--- roximagescan.sh
+++ roximagescan.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 #curl -s -k -H "Authorization: Bearer ${ROX_API_TOKEN}" https://${CENTRAL}/api/cli/download/roxctl-linux --output roxctl
-#chmod +x ./roxctl
+sudo chmod +x ./roxctl
 
 echo "scanning image ${IMAGE} at server ${CENTRAL}"
-./roxctl image check --insecure-skip-tls-verify -e ${CENTRAL} --image ${IMAGE}
+sudo ./roxctl image check --insecure-skip-tls-verify -e ${CENTRAL} --image ${IMAGE}
 
 exit $?
