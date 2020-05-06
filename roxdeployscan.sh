d2a2bdfd4c0e9a5c5417dd06057b54686df737ed
Amari nader <amari.nader@gmail.com>
2020-05-06 12:42:08 -0400

    add ROX sh scripts

---
+++ roxdeployscan.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -s -k -H "Authorization: Bearer ${ROX_API_TOKEN}" https://${CENTRAL}/api/cli/download/roxctl-linux --output roxctl
+chmod +x ./roxctl
+
+#export ROX_API_TOKEN=${ROX_API_TOKEN}
+./roxctl deployment check --insecure-skip-tls-verify -e ${CENTRAL} --file ${KUBEYAML}
+
+exit $?
